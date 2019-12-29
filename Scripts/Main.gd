8c7768f421beac0cc099cd620e6a1d562b35997f
David Lang <langdjii@gmail.com>
2019-12-29 14:53:56 -0600

    initial commit

---
+++ Scripts/Main.gd
@@ -0,0 +1,6 @@
+
+extends Node
+
+func _ready():
+
+	get_tree().change_scene('res://Scenes/Arenas/Test.tscn')
---
+++ Scripts/Pause.gd
@@ -0,0 +1,24 @@
+
+extends Control
+
+onready var pause_state = false
+
+func _input(event):
+	if event.is_action_pressed('ui_cancel'):
+		if pause_state == false:
+			pause_state = true
+			visible = true
+			get_tree().paused = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		elif pause_state == true:
+			pause_state = false
+			visible = false
+			get_tree().paused = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+
+
+
+
+
