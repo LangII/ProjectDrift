9653cf2c82601f9b3fde7e9364aa455a82fe099b
David Lang <langdjii@gmail.com>
2019-12-29 22:02:59 -0600

    in progress ... modularizing vehicle, arena, gameplay build

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,8 @@
 
 extends Node
 
+onready var globals = get_node('/root/Globals')
+
 func _ready():
 
-    get_tree().change_scene('res://Scenes/Arenas/Test.tscn')
+    get_tree().change_scene('res://Scenes/Arenas/' + globals.test_arena + '.tscn')
