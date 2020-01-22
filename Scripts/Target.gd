bd72c80b620a4b745ea81611d3fdd65cdd240599
David Lang <langdjii@gmail.com>
2020-01-21 21:08:20 -0600

    turret's slerp works better

--- Scripts/Target.gd
+++ Scripts/Target.gd
@@ -13,7 +13,9 @@ onready var turret_speed = controls.targets[target_tag]['turret_speed']
 # onready var col_radius = $Visibility/CollisionShape.shape.radius
 
 onready var turret = $Turret
-onready var targeting = null
+onready var obj_visible = null
+onready var targeting
+onready var how_far
 
 onready var rot_tween = $Turret/RotTween
 
@@ -35,24 +37,31 @@ func _process(delta):
     # print("origin - ", turret.global_transform.origin + Vector3.DOWN)
     # print("rotation ", turret.rotation)
 
-    if targeting:
+    """ WORKING """
+    # var targeting_origin = obj_visible.global_transform.origin - turret.global_transform.origin
+    # var targeting_dir = turret.transform.looking_at(targeting_origin, Vector3.UP)
+    # turret.transform = turret.transform.interpolate_with(targeting_dir, 0.01)
 
-        # turret.look_at(targeting.transform.origin - turret.transform.origin, Vector3.UP)
-
-        var targeting_origin = targeting.global_transform.origin - turret.global_transform.origin
-        var targeting_dir = turret.transform.looking_at(targeting_origin, Vector3.UP)
+    """ WORKING BETTER """
+    if obj_visible:  targeting = obj_visible.global_transform.origin - turret.global_transform.origin
+    else:  targeting = Vector3(0, -1, 0.001)
+    var targeting_dir = turret.transform.looking_at(targeting, Vector3.UP)
+    if not turret.transform.basis.is_equal_approx(targeting_dir.basis, 0.01):
         turret.transform = turret.transform.interpolate_with(targeting_dir, 0.01)
 
-    else:
+    # how_far = turret.transform.basis.get_euler().angle_to(targeting_dir.basis.get_euler())
+    # if how_far > 0.1:
 
-        turret.look_at(turret.global_transform.origin + Vector3(0, -1, 0.01), Vector3.UP)
+    # turret.look_at(targeting.transform.origin - turret.transform.origin, Vector3.UP)
+
+    # else:
+
+        # turret.look_at(turret.global_transform.origin + Vector3(0, -1, 0.01), Vector3.UP)
 
 func _on_Visibility_body_entered(body):
 
-    # print(body.name)
-
-    if body.name == 'Vehicle':  targeting = body
+    if body.name == 'Vehicle':  obj_visible = body
 
 func _on_Visibility_body_exited(body):
 
-    if body == targeting:  targeting = null
+    if body == obj_visible:  obj_visible = null
